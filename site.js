cacb0afb9369b287fb16a98f44ebd8e49a095b98
Logan Wilcox <lwilcox@hawk.iit.edu>
2018-01-31 13:28:48 -0600

    Added the code from the first production problem as a starter framework and added a photo to my media folder

---
+++ site.js
@@ -0,0 +1,40 @@
+$('#contact-form').on('submit',
+  function(e) {
+    var phone = $('#phone').val();
+    var email = $('#email').val();
+    var birthday = $('#birthday').val();
+    var check = new Date();
+    var agecheck = check.getYear();
+    var check2 = /\d{10}/;
+    var check3 = /.*@.*/;
+    check = new Date(check.setYear(check.getYear() - 18));
+
+
+    //  Not sure whats wrong with my birthday validation but it's not working....
+    if (!(birthday > agecheck)) {
+      console.log('must be 18 years or older');
+      $('#birthday-input').remove('#warning');
+      $('#birthday-input').append('<li id="warning">must be 18 years or older</li>');
+      return false;
+    }
+    if(!(check2.test(phone)))
+    {
+      console.log('invalid phone number');
+      $('#warning').remove;
+      $('#phone-input').append('<li id="warning">Please enter a valid telephone number</li>');
+      return false;
+    }
+    if(!(check3.test(email)))
+    {
+      console.log('invalid email address');
+      $('#warning').remove;
+      $('#email-input').append('<li id="warning">Please enter a valid email address</li>');
+      return false;
+    }
+    else {
+      e.preventDefault();
+      $(this).remove();
+      $('body').append('<h1>Thank You! Your form has been submitted. Email Address: ' + email + '</h1>');
+    }
+  }
+);
