15a84b1a974f261c987da5df587e74e1a47411ce
Logan Wilcox <lwilcox@hawk.iit.edu>
2018-02-18 19:24:13 -0600

    added to javascript and css so that when the form is submitted with valid information, the form elements dissapear and they are replaced with a thank you / confirmation message. Still need to align them properly because they are too far up on the page

--- site.js
+++ site.js
@@ -2,33 +2,40 @@ $('#contact-form').on('submit',
   function(e) {
     var phone = $('#phone').val();
     var email = $('#email').val();
+    var name = $('#name').val();
     var birthday = $('#birthday').val();
     var check = new Date();
     var agecheck = check.getYear();
     var check2 = /\d{10}/;
-    var check3 = /.*@.*/;
+    var check3 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     check = new Date(check.setYear(check.getYear() - 18));
 
-
-    //  Not sure whats wrong with my birthday validation but it's not working....
-    if(!(check2.test(phone)))
-    {
-      console.log('invalid phone number');
-      $('#phone-warning').remove();
-      $('#phone-input').append('<li id="phone-warning">Please enter a valid telephone number</li>');
-      return false;
-    }
     if(!(check3.test(email)))
     {
-      console.log('invalid email address');
-      $('#warning').remove;
-      $('#email-input').append('<li id="warning">Please enter a valid email address</li>');
-      return false;
-    }
-    else {
       e.preventDefault();
-      $(this).remove();
-      $('body').append('<h1 id="thankyou">Thank You, ' + name + '! Your form has been submitted. Email Address: ' + email + '</h1>');
+      console.log('invalid email address');
+      $('#email-warning').remove();
+      $('#email-input').append('<li id="email-warning">Please enter a valid email address!</li>');
+    }
+    if(!(check2.test(phone)))
+    {
+      e.preventDefault();
+      console.log('invalid phone number');
+      $('#phone-warning').remove();
+      $('#phone-input').append('<li id="phone-warning">Please enter a valid telephone number!</li>');
+    }
+    if(check2.test(phone) && check3.test(email)) {
+      e.preventDefault();
+      $('#team').remove();
+      $('#name-input').remove();
+      $('#email-input').remove();
+      $('#phone-input').remove();
+      $('#submit').remove();
+      $('#learn-more').remove();
+      //$(this).remove();
+      $('form').append('<h1 id="thankyou">Thank You, ' + name + '!</h1>');
+      $('form').append('<h1 id="thankyou">Your form has been submitted.</h1>');
+      $('form').append('<h1 id="thankyou">Email Address: ' + email + '</h1>');
     }
   }
 );
